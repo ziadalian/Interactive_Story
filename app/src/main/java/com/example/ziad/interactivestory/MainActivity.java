c7ee4ff13170b045d65b103a4079ebc85634960b
ziad <ziad@MacBook-Pro.local>
2016-04-28 06:32:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ziad/interactivestory/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.ziad.interactivestory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.StartButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                startStory(name);
+            }
+        });
+
+
+    }
+
+
+    public void startStory(String name) {
+
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/ziad/interactivestory/StoryActivity.java
@@ -0,0 +1,29 @@
+package com.example.ziad.interactivestory;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class StoryActivity extends AppCompatActivity {
+    public static final String Tag = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        if (name == null) {
+            name = "friend";
+        }
+        Log.d(Tag, name);
+    }
+}
